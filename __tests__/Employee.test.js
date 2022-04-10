f715d4c547f6053c7360ac21f65d275520702173
CullerWhale <jcullenharris@gmail.com>
2022-04-10 16:20:41 -0500

    getName fail

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -11,8 +11,34 @@ console.log(new Engineer());
 
 
 const Employee = require('../lib/Employee.js');
+const { expect, test } = require('@jest/globals');
 test ('creates an employee object', () => {
     const employee = new Employee('Cullen');
 
     expect(employee.name).toBe("Cullen");
-})
+
+    expect(employee.engineer).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+
+    expect(employee.getName()).toHaveProperty('name');
+});
+
+
+test('gets name from employee or returns false', () => {
+    const employee = new Employee('Alfred');
+    const employeeName = employee.name;
+
+    // expect(employee.getName()).toEqual(expect.any(Array));
+    expect(employeeName.getName()).toEqual('Alfred');
+
+    // employee.name='';
+    // expect(employee.getName()).toEqual('');
+
+
+    // const testValue = "Alice";
+    // const e = new Employee(testValue);
+    // expect(e.getName()).toBe(testValue);
+
+
+});
